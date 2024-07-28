54404832bfa49e0ed68b7187d779afe782b40d93
Paulo Frossard <paulo.frossard@protonmail.com>
2024-07-28 11:52:40 -0300

    - Basic structure for hits function (incomplete) - Implement random delay to emulate human usage in farm (complete)

--- src/autoFarm.js
+++ src/autoFarm.js
@@ -5,6 +5,7 @@ class AutoFarm extends ModernUtil {
         // Load the settings
         this.timing = this.storage.load('af_level', 300000);
         this.percent = this.storage.load('af_percent', 1);
+        this.hit = this.storage.load('af_hit', 1);
         this.active = this.storage.load('af_active', false);
         this.gui = this.storage.load('af_gui', false);
 
@@ -30,22 +31,32 @@ class AutoFarm extends ModernUtil {
 
         this.$duration = $("<p>Duration:</p>").css({ "text-align": "left", "margin": "2px", "font-weight": "bold" })
         this.$button5 = this.createButton("modern_farm_5", "5 min", this.toggleDuration)
-        this.$button10 = this.createButton("modern_farm_10", "10 min", this.toggleDuration)
-        this.$button20 = this.createButton("modern_farm_20", "20 min", this.toggleDuration)
-        this.$content.append(this.$duration, this.$button5, this.$button10, this.$button20)
+        this.$button10 = this.createButton("modern_farm_10", "10 min", this.toggleDuration).css({ "width": "90px" })
+        this.$button20 = this.createButton("modern_farm_20", "20 min", this.toggleDuration).css({ "width": "90px" })
+        this.$button40 = this.createButton("modern_farm_40", "40 min", this.toggleDuration).css({ "width": "90px" })// Add 40 min
+        this.$button180 = this.createButton("modern_farm_180", "3 Hrs", this.toggleDuration).css({ "width": "80px" }) // Add 3 hours 
+        this.$content.append(this.$duration, this.$button5, this.$button10, this.$button20, this.$button40, this.$button180)
 
         this.$storage = $("<p>Storage:</p>").css({ "text-align": "left", "margin": "2px", "font-weight": "bold" })
-        this.$button80 = this.createButton("modern_farm_80", "80%", this.toggleStorage).css({ "width": "70px" })
+        this.$button70 = this.createButton("morden_farm_70", "70%", this.toggleStorage).css({ "with": "80px" }) // Add 70% 
+        this.$button80 = this.createButton("modern_farm_80", "80%", this.toggleStorage).css({ "width": "80px" })
         this.$button90 = this.createButton("modern_farm_90", "90%", this.toggleStorage).css({ "width": "80px" })
         this.$button100 = this.createButton("modern_farm_100", "100%", this.toggleStorage).css({ "width": "80px" })
-        this.$content.append(this.$storage, this.$button80, this.$button90, this.$button100)
+        this.$content.append(this.$storage, this.$button70, this.$button80, this.$button90, this.$button100)
+
+        this.$hit = $("<p>Farm Hit:</p>").css({ "text-align": "left", "margin": "2px", "font-weight": "bold" })
+        this.$button_hit50 = this.createButton("modern_farm_hit_50", "50%", this.toggleHit).css({ "width": "80px" })
+        this.$button_hit67 = this.createButton("modern_farm_hit_67", "67%", this.toggleHit).css({ "width": "80px" })
+        this.$button_hit83 = this.createButton("modern_farm_hit_83", "83%", this.toggleHit).css({ "width": "80px" })
+        this.$button_hit100 = this.createButton("modern_farm_hit_100", "100%", this.toggleHit).css({ "width": "80px" })
+        this.$content.append(this.$hit, this.$button_hit50, this.$button_hit67, this.$button_hit83, this.$button_hit100)
 
         this.$gui = $("<p>Gui:</p>").css({ "text-align": "left", "margin": "2px", "font-weight": "bold" })
         this.$guiOn = this.createButton("modern_farm_gui_on", "ON", this.toggleGui)
         this.$guiOff = this.createButton("modern_farm_gui_off", "OFF", this.toggleGui)
         this.$content.append(this.$gui, this.$guiOn, this.$guiOff)
 
-        this.$popup = this.createPopup(423, 250, 170, this.$content)
+        this.$popup = this.createPopup(423, 480, 220, this.$content)
         this.dropdown_active = false
 
         // Open and close the dropdown with the mouse
@@ -82,17 +93,37 @@ class AutoFarm extends ModernUtil {
 
     /* Update the buttons */
     updateButtons = () => {
+        // Create button for times
         this.$button5.addClass('disabled')
         this.$button10.addClass('disabled')
         this.$button20.addClass('disabled')
+        this.$button40.addClass('disabled')
+        this.$button180.addClass('disabled')
+
+        // Create button for max storage
+        this.$button70.addClass('disabled')
         this.$button80.addClass('disabled')
         this.$button90.addClass('disabled')
         this.$button100.addClass('disabled')
 
+        // Create button for hit to farm
+        this.$button_hit50.addClass('disabled')
+        this.$button_hit67.addClass('disabled')
+        this.$button_hit83.addClass('disabled')
+        this.$button_hit100.addClass('disabled')
+
+        if (this.percent_hit == 0.5) this.$button_hit50.removeClass('disabled')
+        if (this.percent_hit == 0.67) this.$button_hit67.removeClass('disabled')
+        if (this.percent_hit == 0.83) this.$button_hit83.removeClass('disabled')
+        if (this.percent_hit == 1) this.$button_hit100.removeClass('disabled')
+
         if (this.timing == 300000) this.$button5.removeClass('disabled')
         if (this.timing == 600000) this.$button10.removeClass('disabled')
         if (this.timing == 1200000) this.$button20.removeClass('disabled')
+        if (this.timing == 2400000) this.$button40.removeClass('disabled') // add 40min
+        if (this.timing == 10800000) this.$button180.removeClass('disabled') // add 3h
 
+        if (this.percent == 0.7) this.$button70.removeClass('disabled') // Add button 70%
         if (this.percent == 0.8) this.$button80.removeClass('disabled')
         if (this.percent == 0.9) this.$button90.removeClass('disabled')
         if (this.percent == 1) this.$button100.removeClass('disabled')
@@ -108,6 +139,18 @@ class AutoFarm extends ModernUtil {
         else this.$guiOff.removeClass('disabled')
     }
 
+    toggleHit = (event) => {
+        const { id } = event.currentTarget
+        if (id == "modern_farm_hit_50") this.percent_hit = 0.5
+        if (id == "modern_farm_hit_67") this.percent_hit = 0.67
+        if (id == "modern_farm_hit_83") this.percent_hit = 0.83
+        if (id == "modern_farm_hit_100") this.ppercent_hit = 1
+
+        // Save the settings and update the buttons
+        this.storage.save('af_hit', this.percent_hit);
+        this.updateButtons()
+    }
+
     toggleDuration = (event) => {
         const { id } = event.currentTarget
 
@@ -115,6 +158,8 @@ class AutoFarm extends ModernUtil {
         if (id == "modern_farm_5") this.timing = 300_000
         if (id == "modern_farm_10") this.timing = 600_000
         if (id == "modern_farm_20") this.timing = 1_200_000
+        if (id == "modern_farm_40") this.timing = 2_400_000 // Add time 40 min
+        if (id == "modern_farm_180") this.timing = 10_800_000 // Add time 3h
 
         // Save the settings and update the buttons
         this.storage.save('af_level', this.timing);
@@ -125,6 +170,7 @@ class AutoFarm extends ModernUtil {
         const { id } = event.currentTarget
 
         // Update the percent
+        if (id == "morden_farm_70") this.percent = 0.7 // Add farm 70%
         if (id == "modern_farm_80") this.percent = 0.8
         if (id == "modern_farm_90") this.percent = 0.9
         if (id == "modern_farm_100") this.percent = 1
@@ -206,9 +252,9 @@ class AutoFarm extends ModernUtil {
             maxLootableTime = lootableTime;
             maxValue = value;
         }
-
         const seconds = maxLootableTime - Math.floor(Date.now() / 1000);
         return seconds > 0 ? seconds * 1000 : 0;
+
     };
 
     /* Call to update the timer */
@@ -230,9 +276,9 @@ class AutoFarm extends ModernUtil {
         // If the captain is active, claim all the resources at once and fake the opening
         if (isCaptainActive && !this.gui) {
             await this.fakeOpening();
-            await this.sleep(Math.random() * 2000 + 1000); // random between 1 second and 3
+            await this.sleep(Math.random() * 4000 + 1000); // random between 1 second and 3
             await this.fakeSelectAll();
-            await this.sleep(Math.random() * 2000 + 1000);
+            await this.sleep(Math.random() * 4000 + 1000);
             if (this.timing <= 600_000) await this.claimMultiple(300, 600);
             if (this.timing > 600_000) await this.claimMultiple(1200, 2400);
             await this.fakeUpdate();
@@ -246,17 +292,25 @@ class AutoFarm extends ModernUtil {
             return;
         }
 
-        // If the captain is not active, claim the resources one by one, but limit the number of claims
+        // If the captain is not active, claim the resources one by one, but limit the number of claims 
+        // Human simulate click - Using random time
+        let min_sleep = 2000
+        let max_sleep = 15000
+        let sleep_time = Math.random() * 10000 + 5000
+        console.log("Sleep_time " + sleep_time)
         let max = 60;
         const { models: player_relation_models } = uw.MM.getOnlyCollectionByName('FarmTownPlayerRelation');
         const { models: farm_town_models } = uw.MM.getOnlyCollectionByName('FarmTown');
         const now = Math.floor(Date.now() / 1000);
         for (let town_id of polis_list) {
+            console.log("tower_id: \n" + town_id)
             let town = uw.ITowns.towns[town_id];
             let x = town.getIslandCoordinateX();
             let y = town.getIslandCoordinateY();
 
             for (let farm_town of farm_town_models) {
+                console.log("Farm number: " + farm_town_models.length)
+                console.log("Farm town: \n" + farm_town)
                 if (farm_town.attributes.island_x != x) continue;
                 if (farm_town.attributes.island_y != y) continue;
 
@@ -266,7 +320,7 @@ class AutoFarm extends ModernUtil {
                     if (relation.attributes.lootable_at !== null && now < relation.attributes.lootable_at) continue;
 
                     this.claimSingle(town_id, relation.attributes.farm_town_id, relation.id, Math.ceil(this.timing / 600_000));
-                    await this.sleep(500);
+                    await this.sleep(sleep_time);
                     if (!max) return;
                     else max -= 1;
                 }
